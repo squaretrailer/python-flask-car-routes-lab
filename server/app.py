1c6f6664df1c2eda82538396009c016fe495c72a
Christian <your_email@example.com>
2026-06-24 08:42:24 +0300

    Fix app.py: implement Flask routes per spec

--- server/app.py
+++ server/app.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+@app.route("/")
+def index():
+    return 'Welcome to Flatiron Cars'
+
+@app.route("/<string:model>")
+def model_lookup(model):
+    if model in existing_models:
+        return f'Flatiron {model} is in our fleet!'
+    return f'No models called {model} exists in our catalog'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
